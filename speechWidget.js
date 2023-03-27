b95b25a4f399c2b2ccb1a2539ca27ee56961c19c
dzdidi <deniszalessky@gmail.com>
2023-03-27 20:06:39 +0200

    Example: speech widget clean up
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- speechWidget.js
+++ speechWidget.js
@@ -4,11 +4,25 @@ import { SDK, SlashURL } from '@synonymdev/slashtags-sdk'
 
 const sdk = new SDK()
 const slashtag = sdk.slashtag('demo')
-
 const drive = slashtag.drivestore.get()
 await drive.ready()
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.on('close', async () => {
+  await drive.close()
+  console.log('Input stream closed.')
+  process.exit()
+})
+
+const path = '/feed/text'
+
 console.log('Convert me to QR and scan with bitkit:', SlashURL.format(drive.key, { protocol: 'slashfeed:' }))
+
+await drive.put(path, Buffer.from('hello world'))
 
 const img = fs.readFileSync('./image.bs64', 'utf-8')
 await drive.put('/slashfeed.json', Buffer.from(JSON.stringify({
@@ -18,25 +32,14 @@ await drive.put('/slashfeed.json', Buffer.from(JSON.stringify({
   fields: [
     {
       name: 'to all of you',
-      main: '/feed/text/'
+      main: path
     }
   ]
 })))
 
-await drive.put('/feed/text/', Buffer.from('hello world'))
-
 console.log('Type something below and hit Enter:')
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
 rl.on('line', async (line) => {
+  await drive.put(path, Buffer.from(line))
   console.log('Type something below and hit Enter:')
-  await drive.put('/feed/text/', Buffer.from(line))
 })
 
-rl.on('close', async () => {
-  await drive.close()
-  console.log('Input stream closed.')
-  process.exit()
-})
